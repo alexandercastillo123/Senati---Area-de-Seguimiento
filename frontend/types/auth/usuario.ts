e8ba7c098db3667c3a382b03469e5fd9b21b1c4e
edison2861 <64335516+edison2861@users.noreply.github.com>
2026-05-05 11:23:31 -0500

    Zod y react hook form

---
+++ frontend/types/auth/usuario.ts
@@ -0,0 +1,11 @@
+// Se colocaran todas las interfaces relacionadas al usuario, como el login, etc
+export interface Usuario {
+    email: string;
+    password: string;
+}
+
+
+export interface UsuarioLogin {
+    email: string;
+    password: string;
+}
